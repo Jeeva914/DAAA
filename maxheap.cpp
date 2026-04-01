5c13aabb138b383147780963eabe971cd2efafc7
Jeeva914 <jeevasonu914@gmail.com>
2026-04-01 11:22:47 +0530

    Create maxheap.cpp

---
+++ maxheap.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(int arr[], int n) {
+    for (int i = n/2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
+
+int main() {
+    int arr[] = {3, 9, 2, 1, 4, 5};
+    int n = 6;
+
+    buildMaxHeap(arr, n);
+
+    cout << "Max Heap: ";
+    for(int i=0;i<n;i++)
+        cout << arr[i] << " ";
+}
